f40afe3a12713a9f9951ffff0c73109f3627ac52
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-24 14:47:49 -0400

    created card elements for page

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 //document.addEventListener("DOMContentLoaded",fetchBreweryData())
 
-
 //function fetchBreweryData() {
-
-  
   fetch ('https://api.openbrewerydb.org/breweries')
   .then(response => response.json())
-  .then( breweryArray => {
+  .then(breweryArray => {
     
     breweryArray.forEach((brewery) => {renderBreweryData(brewery)
     
-      
     })
     //console.log(breweryArray)
   })
-    
+
+  // function to generate random brewery name at page load
+  function renderBreweryData(brewery){
+    console.log(brewery)
+    let breweryContainer = document.querySelector('#breweryContainer')
+    let breweryCards = document.createElement('div')
+    breweryCards.classList.add('card')
+    breweryContainer.append(breweryCards)
+  }
   
-  
-  document.querySelector("#comment-form")
-  .addEventListener("submit", e => {
+  // comment form function
+  const comments = document.querySelector('#comment-form')
+
+  comments.addEventListener("submit", e => {
     e.preventDefault()
     const breweryComment = document.createElement('li')
     breweryComment.textContent = e.target['review'].value
     // console.log(e.target['review'].value)
-    
     const reviews = document.querySelector('#reviews')
     //reviews.value.reset()
       reviews.appendChild(breweryComment)
-      
     })
     //}
-    
-    function renderBreweryData(brewery){
-      console.log(brewery)
 
-    }
+    // create search form to filter by location
+    
