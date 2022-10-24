e4abe65fcacbecbe2ebc010a4b0365b6535dbe71
Nick Landas <nick.landas@gmail.com>
2022-10-24 10:12:04 -0500

    Added review functinality with bugs

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ fetch ('https://api.openbrewerydb.org/breweries')
 .then(response => response.json())
 .then(data => (data))
 
-// create review form
+document.querySelector("#comment-form")
+ .addEventListener("submit", e => {
+    e.preventDefault()
+    const breweryComment = document.createElement('li')
+    breweryComment.textContent = e.target['review'].value
+    // console.log(e.target['review'].value)
+    const reviews = document.querySelector('#reviews')
+    reviews.innerHTML = ""
+      reviews.appendChild(breweryComment)
+ })
