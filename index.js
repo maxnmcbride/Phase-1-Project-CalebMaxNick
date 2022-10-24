42d3576bb97160770c0cb3b5c7d851141c231989
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-24 10:47:01 -0500

    add a for each.

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-fetch ('https://api.openbrewerydb.org/breweries')
-.then(response => response.json())
-.then(data => (data))
+//document.addEventListener("DOMContentLoaded",fetchBreweryData())
 
-document.querySelector("#comment-form")
- .addEventListener("submit", e => {
+
+//function fetchBreweryData() {
+
+  
+  fetch ('https://api.openbrewerydb.org/breweries')
+  .then(response => response.json())
+  .then( breweryArray => {
+    
+    breweryArray.forEach((brewery) => {renderBreweryData(brewery)
+    
+      
+    })
+    //console.log(breweryArray)
+  })
+    
+  
+  
+  document.querySelector("#comment-form")
+  .addEventListener("submit", e => {
     e.preventDefault()
     const breweryComment = document.createElement('li')
     breweryComment.textContent = e.target['review'].value
     // console.log(e.target['review'].value)
+    
     const reviews = document.querySelector('#reviews')
-    reviews.innerHTML = ""
+    //reviews.value.reset()
       reviews.appendChild(breweryComment)
- })
+      
+    })
+    //}
+    
+    function renderBreweryData(brewery){
+      console.log(brewery)
+
+    }
